7d8fb9b0351b9b043c0071db1f5d8f3ad4cc9aa4
febaasharaf-lgtm <asharaffeba@gmail.com>
2025-06-22 11:21:55 +0530

    Create StorageUtil.java

---
+++ StorageUtil.java
@@ -0,0 +1,9 @@
+public class StorageUtil {
+    public static void saveTasks(List<Task> tasks) {
+        // Serialize to tasks.json using Gson or Jackson
+    }
+
+    public static List<Task> loadTasks() {
+        // Deserialize from tasks.json
+    }
+}
